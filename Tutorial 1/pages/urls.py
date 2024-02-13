560e5529c57106e085c5d48c3d2c3a770b7b1ef8
JoseDavid10190307 <jdvalenci2@eafit.edu.co>
2024-02-12 19:49:57 -0500

    Terminado

--- Tutorial 1/pages/urls.py
+++ Tutorial 1/pages/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import HomePageView
-	
+from .views import homePageView, AboutPageView, contactPageView, ProductIndexView, ProductShowView, ProductCreateView
 	
 urlpatterns = [
-    path("", HomePageView.as_view(), name='home')
+    path("", homePageView.as_view(), name='home'),
+    path('about/', AboutPageView.as_view(), name='about'),
+    path('contact/', contactPageView.as_view(), name='contact'),
+    path('products/', ProductIndexView.as_view(), name='index'),
+    path('products/create', ProductCreateView.as_view(), name='form'),
+    path('products/<str:id>', ProductShowView.as_view(), name='show'),
 ]
